e1800cd40dec70439aa7c5f87c82b260000bc520
ehsan.ghasaei@gmail.com <ehsan.ghasaei@gmail.com>
2020-03-04 16:59:27 -0800

    Whole code is changed to Sortable.js

--- funcions.js
+++ funcions.js
@@ -1,25 +1,27 @@
-const drag = (ev) => {
-    console.log("drag test")
-    ev.dataTransfer.setData("text" , ev.target.id)
+// Used for from scratch drag and drop
+// const drag = (ev) => {
+//     console.log("drag test");
+//     console.log(ev);
+//     ev.dataTransfer.setData("text" , ev.target.id)
     
-    let rowid = document.getElementById("row1");
-    rowid.style.fontStyle = "italic";
-    let crowid = rowid.cloneNode(true);
-    //console.log("crowid:",crowid);
-    let inner = crowid.getElementByClassName("rowclass");
-    console.log("inner name" , inner);
-}
+//     let rowid = document.getElementById("row1");
+//     rowid.style.fontStyle = "italic";
+//     let crowid = rowid.cloneNode(true);
+//     //console.log("crowid:",crowid);
+//     let inner = crowid.getElementByClassName("rowclass");
+//     console.log("inner name" , inner);
+// }
 
-const AllowDrop = (ev) => {
-    // console.log("test",ev)
-    ev.preventDefault();
-}
+// const AllowDrop = (ev) => {
+//     // console.log("test",ev)
+//     ev.preventDefault();
+// }
 
-const Drop = (ev) => {
-    ev.preventDefault();
-    data = ev.dataTransfer.getData("text");
-    console.log("test",ev);
-    document.getElementById("targetbody").appendChild(document.getElementById(data));
-    document.getElementById("row1").style.fontStyle = "normal"
-    //ev.target.appendChild(document.getElementById(data));
-}
+// const Drop = (ev) => {
+//     ev.preventDefault();
+//     data = ev.dataTransfer.getData("text");
+//     console.log("test",ev);
+//     document.getElementById("targetbody").appendChild(document.getElementById(data));
+//     document.getElementById("row1").style.fontStyle = "normal"
+//     //ev.target.appendChild(document.getElementById(data));
+// }
